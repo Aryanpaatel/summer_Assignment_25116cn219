93dff6d20fe12848c61e628b260a73be8f0995a7
Aryanpaatel <rn2020202020patel@gmail.com>
2026-06-02 21:28:23 +0530

    Q2.cpp

--- day_1/Q2.cpp
+++ day_1/Q2.cpp
@@ -1,10 +1,10 @@
-//Write a program to Print multiplication table of a given number 
+
 #include <iostream>
 using namespace std;
 int main () {
     int i = 1 , n;
-      cout << (" enter the number : "); // giving output
-      cin >> n ; // taking input
+      cout << (" enter the number : ");
+      cin >> n ; 
        
       for ( i = 1 ; i <=10 ; i++) {
        cout << n << " * " << i << " = " << n * i << endl;
