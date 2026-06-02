bb1a3b3ec2e92ad5a4dd8f30f22f6952d9f57dbd
Aryanpaatel <rn2020202020patel@gmail.com>
2026-06-02 21:27:38 +0530

    Q1.cpp

--- day_1/Q1.cpp
+++ day_1/Q1.cpp
@@ -1,11 +1,11 @@
-//Calculate Sum of First N Natural Numbers
+
 #include <iostream>
 using namespace std;
 int main(){
     int i = 1 , sum = 0 , n ;
-    cout << (" enter the number : "); // giving output
-    cin >> n ; // taking input form user
-     for ( i = 1 ; i <=n ; i++){ // using for loop
+    cout << (" enter the number : "); 
+    cin >> n ; 
+     for ( i = 1 ; i <=n ; i++){ 
         sum = sum + i ;
      }
      cout << (" sum of first n natural number : ") << sum << endl;
