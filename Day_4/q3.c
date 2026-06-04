6c60f054a20d35ae2dc149c2259e53b74e5544c1
Aryanpaatel <rn2020202020patel@gmail.com>
2026-06-04 22:57:19 +0530

    q3.c

--- Day_4/q3.c
+++ Day_4/q3.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main() {
+    int i, n, m, digit, sum;
+    
+    printf("Enter the value of m: ");
+    scanf("%d", &m);
+    
+    
+    for(i = 1; i <= m; i++) {
+        n = i;
+        sum = 0;
+    
+        while(n != 0) {
+            digit = n % 10;
+            sum = sum + (digit * digit * digit);
+            n = n / 10;
+        }
+        
+          if(i == sum) {
+            printf("%d ", i);
+        }
+    }
+    getch();
+}
 
